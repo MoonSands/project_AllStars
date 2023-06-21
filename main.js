ad4ef3ec306a3f90a6e90f0d2dde2ba03841fb40
MoonSands <kfmty135shk@gmail.com>
2023-06-21 13:41:01 +0300

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+function studio_bg(){
+    let studio_elem = document.querySelector('.main-studio');
+    let bgs = document.getElementsByClassName('studio_bg');
+    studio_elem.style.backgroundImage=`linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,rgba(0,0,0,0.5) 100%),url(${bgs[0].getAttribute('src')})`;
+    var i=1;
+    let timerfunc=function(a,b){
+        a.style.backgroundImage = `linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,rgba(0,0,0,0.5) 100%),url(${b[i].getAttribute('src')})` ;
+        (i < b.length) ? i++ : i=0 ;
+    }
+    setInterval(timerfunc, 10000, studio_elem, bgs); 
+}
