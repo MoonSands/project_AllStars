2120c1c06d6592b0d72a24f640466c11e8557618
MoonSands <kfmty135shk@gmail.com>
2023-06-22 19:05:49 +0300

    main page raw

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function studio_bg(){
     var i=1;
     let timerfunc=function(a,b){
         a.style.backgroundImage = `linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,rgba(0,0,0,0.5) 100%),url(${b[i].getAttribute('src')})` ;
-        (i < b.length) ? i++ : i=0 ;
+        (i < b.length-1) ? i++ : i=0 ;
     }
     setInterval(timerfunc, 10000, studio_elem, bgs); 
 }
